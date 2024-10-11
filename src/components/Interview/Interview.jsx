82f6eff794145a052e43b1a0f1f611ce4d5204c9
Bhuwan Bhashkar <nigamrai666@gmail.com>
2024-10-11 15:06:40 +0530

    Updated the Interface of Prep Page

--- src/components/Interview/Interview.jsx
+++ src/components/Interview/Interview.jsx
@@ -1,24 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Loader from '../Loader/Loader.jsx'; // Import the reusable Loader component
-import './interviewCss.css'; // Ensure the Interview-specific CSS is imported
+import Loader from '../Loader/Loader.jsx';
+import './interviewCss.css';
 
 const Interview = () => {
   const [cards, setCards] = useState([]);
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
 
-  // Fetch data from Google Sheets API
   useEffect(() => {
-    fetch('https://script.google.com/macros/s/AKfycbxyb0KmZFC8792N26PvzyboFqzSKiEZpaSU0EQwl4Thmx7wqD7YAafpxrQU7U9EEuon/exec')
+    fetch('https://script.google.com/macros/s/AKfycbzI_rB4uIWVOIlrb0nBbY7yGEj2jn0hIPDkob1mbaKISnRsvNpRDkzetvF7Ui57veNU/exec')
       .then((response) => response.json())
       .then((data) => {
         setCards(data);
-        setLoading(false); // Set loading to false once data is fetched
+        setLoading(false);
       })
       .catch((error) => {
         console.error('Error fetching data:', error);
-        setLoading(false); // Set loading to false on error
+        setLoading(false);
       });
   }, []);
 
@@ -28,21 +27,30 @@ const Interview = () => {
 
   return (
     <>
-      {/* Conditionally render loader or content */}
       {loading ? (
-        <Loader />// Use the reusable Loader component here
+        <Loader />
       ) : (
         <div className="container">
           <h1 className="title">Courses</h1>
           <div className="cards">
-            {cards.map((card, index) => (
-              <div key={index} className="card" onClick={() => handleClick(card)}>
-                <div className="card-body">
-                  <h3>{card.name}</h3>
-                  <p>Click to view details</p>
+            {cards.map((card, index) => {
+              console.log("Image URL:", card.image); // Add this line for debugging
+              return (
+                <div key={index} className="card" onClick={() => handleClick(card)}>
+                  <div className="card-body">
+                    {card.image ? (
+                      <img src={card.image} alt={`${card.name} image`} className="card-image" />
+                    ) : (
+                      <p>No image available</p>
+                    )}
+
+                    {/* <h3>{card.name}</h3> */}
+                    <p>Click to view details</p>
+                  </div>
                 </div>
-              </div>
-            ))}
+              );
+            })}
+
           </div>
         </div>
       )}
