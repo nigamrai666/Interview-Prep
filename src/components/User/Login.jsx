7eca457f3a898f6540949ef04cbbee0de5437133
Bhuwan Bhashkar <nigamrai666@gmail.com>
2026-02-21 00:29:43 +0530

    corrected the import of css file in login and register component

--- src/components/User/Login.jsx
+++ src/components/User/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import "./auth.css";
+import "./Auth.css";
 
 export default function Login() {
   const [email, setEmail] = useState("");
